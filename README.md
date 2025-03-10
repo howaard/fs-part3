e77822d763e8722043d58ec4d351d6ded77e01da
unknown <chesterccj2@hotmail.com>
2025-03-10 21:06:55 +1100

    added frontend

---
+++ README.md
@@ -0,0 +1,1 @@
+https://fs-part3-kkum.onrender.com
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 // Custom Morgan token to log request body for POST requests
 morgan.token('body', (req) => {
